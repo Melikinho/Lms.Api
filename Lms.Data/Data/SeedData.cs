2968f24164b3b31e93042dc0676c4ad21fe50a0a
Melikinho <Jimmymelik10@gmail.com>
2022-08-31 11:34:50 +0200

    Updating SeedData with new courses and modules

--- Lms.Data/Data/SeedData.cs
+++ Lms.Data/Data/SeedData.cs
@@ -1,4 +1,6 @@
-﻿using Lms.Api.Data;
+﻿using Bogus;
+using Lms.Api.Data;
+using Lms.Core.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,17 +12,68 @@ namespace Lms.Data.Data
 {
     public class SeedData
     {
-        private static LmsApiContext db;
+        private static LmsApiContext db = default!;
         public static async Task InitAsync(LmsApiContext context)
         {
+            //var faker = new Faker("sv");
+            //var courses = new List<Course>();
+
             if (context == null) throw new ArgumentNullException(nameof(context));
             db = context;
 
+            var courses = GetCourses();
+            await db.Course.AddRangeAsync(courses);
+
+            var modules = GetModules();
+            await db.Module.AddRangeAsync();
+
+
             if (await db.Course.AnyAsync()) return;
+
+            await db.SaveChangesAsync();
         }
 
+        private static IEnumerable<Module> GetModules()
+        {
+            return new[]
+            {
+            new Module()
+            {
+                Id = 1,
+                Title = "Programming",
+                StartDate = DateTime.Now,
+                CourseId = 1
+            },
+            new Module()
+            {
+                Id = 2,
+                Title = "Language",
+                StartDate = DateTime.Now,
+                CourseId = 2
+            }
 
+        };
 
+        }
 
+        private static IEnumerable<Course> GetCourses()
+        {
+            return new[]
+            {
+                new Course()
+            {
+                Id = 1,
+                Title = "Programming Course",
+                StartDate = DateTime.Today.AddDays(3).AddHours(7)
+            },
+                new Course()
+                {
+                    Id = 2,
+                    Title = "Math Course",
+                    StartDate = DateTime.Today.AddDays(2).AddHours(6)
+                }
+        };
+
+        }
     }
 }
