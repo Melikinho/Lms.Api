93363db0e8c8850e227e812842e541da59b6b41b
Melikinho <Jimmymelik10@gmail.com>
2022-08-31 10:07:28 +0200

    Adding Extensions

---
+++ Lms.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Lms.Api.Data;
+using Lms.Data.Data;
+using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Lms.Api.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static async Task<IApplicationBuilder>SeedDataAsync(this IApplicationBuilder application)
+        {
+            using (var scope = application.ApplicationServices.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var db = serviceProvider.GetRequiredService<LmsApiContext>();
+                db.Database.EnsureCreated();
+                db.Database.Migrate();
+
+                try
+                {
+                    await SeedData.InitAsync(db);
+                }
+                catch(Exception e)
+                {
+                    throw;
+                }
+            }
+
+            return application;
+        }         
+
+    }
+
+}
---
+++ Lms.Data/Data/SeedData.cs
@@ -0,0 +1,26 @@
+﻿using Lms.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Data.Data
+{
+    public class SeedData
+    {
+        private static LmsApiContext db;
+        public static async Task InitAsync(LmsApiContext context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            db = context;
+
+            if (await db.Course.AnyAsync()) return;
+        }
+
+
+
+
+    }
+}
