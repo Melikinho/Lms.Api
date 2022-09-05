1d58606c805103051413f483f3e0e48eeb1b04bf
HP <HP@PC>
2022-09-05 11:43:48 +0200

    Adding Dto folder with CourseDto & ModuleDto with its properties

---
+++ Lms.Core/Dto/CourseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Core.Lms.Core.Dto
+{
+#nullable disable
+    public class CourseDto
+    {
+        public string TitleDto { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime StopDate => StartDate.AddMonths(3);
+    }
+}
